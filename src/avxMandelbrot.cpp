5b7078f47e73e919cf4df9fd8e9c89695c238c95
lvbealr <lvbealr@mail.ru>
2025-04-01 12:57:16 +0300

    blue and pink gradient

--- src/avxMandelbrot.cpp
+++ src/avxMandelbrot.cpp
@@ -49,107 +49,107 @@ void hsvToRgb(float h, float s, float v, uint8_t& r, uint8_t& g, uint8_t& b) {
     b = (uint8_t)((b1 + m) * 255.0f);
 }
 
-renderError generateColor(uint8_t *points, const float x, const float y, const float iter) {
-    if (!points) return NULL_PTR;
-
-    size_t pointPos = (size_t)(y * WIDTH + x) * 4;
-    float t = iter / (float)(ITER_MAX - 1);
-
-    // black&white waves
-    uint8_t intensity = (uint8_t)(127.5f * (1 + cosf(255.0f * t)));
-
-    points[pointPos]     = intensity;
-    points[pointPos + 1] = intensity;
-    points[pointPos + 2] = intensity;
-    points[pointPos + 3] = 255;
-
-    return NO_ERRORS;
-}
-
 //renderError generateColor(uint8_t *points, const float x, const float y, const float iter) {
-//    customAssert(points != NULL, NULL_PTR);
+//    if (!points) return NULL_PTR;
 //
 //    size_t pointPos = (size_t)(y * WIDTH + x) * 4;
-//    float threshold = 0.05f * ITER_MAX;
+//    float t = iter / (float)(ITER_MAX - 1);
 //
-//    if (iter == ITER_MAX || iter < threshold) {
-//        points[pointPos]     = 20;
-//        points[pointPos + 1] = 20;
-//        points[pointPos + 2] = 20;
-//    } else {
-//        float t = iter / (float)(ITER_MAX - 1);
+//    // black&white waves
+//    uint8_t intensity = (uint8_t)(127.5f * (1 + cosf(255.0f * t)));
 //
-//        // base color
-//        float baseHue = 128.0f;
-//        float hueVariation = 5.0f;
-//        float saturation = 0.8f;
-//
-//        // more value more brightness
-//        float value = 0.8f + 0.2f * t;
-//        float hue = baseHue + hueVariation * cosf(256.0f * t);
-//
-//        uint8_t r_t, g_t, b_t;
-//        hsvToRgb(hue, saturation, value, r_t, g_t, b_t);
-//
-//
-//        float glow = 0.1f + 0.7f * sinf(40.0f * t);
-//        uint8_t r = (uint8_t)(r_t * (1.0f - glow) + 128 * glow);
-//        uint8_t g = (uint8_t)(g_t * (1.0f - glow) + 128 * glow);
-//        uint8_t b = (uint8_t)(b_t * (1.0f - glow) + 128 * glow);
-//
-//        // 3d light
-//        float lightDirX = -0.7f;
-//        float lightDirY = -0.7f;
-//        float lightMag = sqrtf(lightDirX * lightDirX + lightDirY * lightDirY);
-//        lightDirX /= lightMag;
-//        lightDirY /= lightMag;
-//
-//        float centerX = WIDTH / 2.0f;
-//        float centerY = HEIGHT / 2.0f;
-//        float normX = (x - centerX) / centerX;
-//        float normY = (y - centerY) / centerY;
-//
-//        float angle   = atan2f(normY, normX);
-//        float normalX = cosf(angle);
-//        float normalY = sinf(angle);
-//
-//        float shading = normalX * lightDirX + normalY * lightDirY;
-//        shading = 0.1f + 0.5f * shading;
-//
-//        r = (uint8_t)fminf(255.0f, r * shading);
-//        g = (uint8_t)fminf(255.0f, g * shading);
-//        b = (uint8_t)fminf(255.0f, b * shading);
-//
-//        float distX = fmin(x, WIDTH - x);
-//        float distY = fmin(y, HEIGHT - y);
-//        float edgeDist = fmin(distX, distY);
-//
-//        float borderThreshold = 65536.0f;
-//        float edgeGlowFactor = 1.0f - fmin(edgeDist / borderThreshold, 1.0f);
-//
-//        // glow gradient
-//        // blue (from left)  RGB = (0, 150, 255)
-//        // pink (from right) RGB = (255, 15, 192)
-//
-//        float gradientFactor = x / (float) WIDTH;
-//
-//        uint8_t tintR = (uint8_t)(0 * (1.0f - gradientFactor) + 255  * gradientFactor);
-//        uint8_t tintG = (uint8_t)(150  * (1.0f - gradientFactor) + 15   * gradientFactor);
-//        uint8_t tintB = (uint8_t)(255 * (1.0f - gradientFactor) + 192  * gradientFactor);
-//
-//        // interpolate
-//        r = (uint8_t)(r * (1.0f - edgeGlowFactor) + tintR * edgeGlowFactor);
-//        g = (uint8_t)(g * (1.0f - edgeGlowFactor) + tintG * edgeGlowFactor);
-//        b = (uint8_t)(b * (1.0f - edgeGlowFactor) + tintB * edgeGlowFactor);
-//
-//        points[pointPos]     = r;
-//        points[pointPos + 1] = g;
-//        points[pointPos + 2] = b;
-//    }
-//
+//    points[pointPos]     = intensity;
+//    points[pointPos + 1] = intensity;
+//    points[pointPos + 2] = intensity;
 //    points[pointPos + 3] = 255;
+//
 //    return NO_ERRORS;
 //}
+
+renderError generateColor(uint8_t *points, const float x, const float y, const float iter) {
+    customAssert(points != NULL, NULL_PTR);
+
+    size_t pointPos = (size_t)(y * WIDTH + x) * 4;
+    float threshold = 0.05f * ITER_MAX;
+
+    if (iter == ITER_MAX || iter < threshold) {
+        points[pointPos]     = 20;
+        points[pointPos + 1] = 20;
+        points[pointPos + 2] = 20;
+    } else {
+        float t = iter / (float)(ITER_MAX - 1);
+
+        // base color
+        float baseHue = 128.0f;
+        float hueVariation = 5.0f;
+        float saturation = 0.8f;
+
+        // more value more brightness
+        float value = 0.8f + 0.2f * t;
+        float hue = baseHue + hueVariation * cosf(256.0f * t);
+
+        uint8_t r_t, g_t, b_t;
+        hsvToRgb(hue, saturation, value, r_t, g_t, b_t);
+
+
+        float glow = 0.1f + 0.7f * sinf(40.0f * t);
+        uint8_t r = (uint8_t)(r_t * (1.0f - glow) + 128 * glow);
+        uint8_t g = (uint8_t)(g_t * (1.0f - glow) + 128 * glow);
+        uint8_t b = (uint8_t)(b_t * (1.0f - glow) + 128 * glow);
+
+        // 3d light
+        float lightDirX = -0.7f;
+        float lightDirY = -0.7f;
+        float lightMag = sqrtf(lightDirX * lightDirX + lightDirY * lightDirY);
+        lightDirX /= lightMag;
+        lightDirY /= lightMag;
+
+        float centerX = WIDTH / 2.0f;
+        float centerY = HEIGHT / 2.0f;
+        float normX = (x - centerX) / centerX;
+        float normY = (y - centerY) / centerY;
+
+        float angle   = atan2f(normY, normX);
+        float normalX = cosf(angle);
+        float normalY = sinf(angle);
+
+        float shading = normalX * lightDirX + normalY * lightDirY;
+        shading = 0.1f + 0.5f * shading;
+
+        r = (uint8_t)fminf(255.0f, r * shading);
+        g = (uint8_t)fminf(255.0f, g * shading);
+        b = (uint8_t)fminf(255.0f, b * shading);
+
+        float distX = fmin(x, WIDTH - x);
+        float distY = fmin(y, HEIGHT - y);
+        float edgeDist = fmin(distX, distY);
+
+        float borderThreshold = 65536.0f;
+        float edgeGlowFactor = 1.0f - fmin(edgeDist / borderThreshold, 1.0f);
+
+        // glow gradient
+        // blue (from left)  RGB = (0, 150, 255)
+        // pink (from right) RGB = (255, 15, 192)
+
+        float gradientFactor = x / (float) WIDTH;
+
+        uint8_t tintR = (uint8_t)(0 * (1.0f - gradientFactor) + 255  * gradientFactor);
+        uint8_t tintG = (uint8_t)(150  * (1.0f - gradientFactor) + 15   * gradientFactor);
+        uint8_t tintB = (uint8_t)(255 * (1.0f - gradientFactor) + 192  * gradientFactor);
+
+        // interpolate
+        r = (uint8_t)(r * (1.0f - edgeGlowFactor) + tintR * edgeGlowFactor);
+        g = (uint8_t)(g * (1.0f - edgeGlowFactor) + tintG * edgeGlowFactor);
+        b = (uint8_t)(b * (1.0f - edgeGlowFactor) + tintB * edgeGlowFactor);
+
+        points[pointPos]     = r;
+        points[pointPos + 1] = g;
+        points[pointPos + 2] = b;
+    }
+
+    points[pointPos + 3] = 255;
+    return NO_ERRORS;
+}
 
 renderError calculateMandelbrot(sf::RenderWindow *window, uint8_t *points,
                                 const float xShift, const float yShift, const float scale) {
