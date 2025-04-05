05f592b05e8d680247df4c9a9b9abb1dda34f9ac
lvbealr <lvbealr@mail.ru>
2025-04-05 07:01:59 +0300

    fix codestyle, check button to show fps

--- include/avxMandelbrot.h
+++ include/avxMandelbrot.h
@@ -9,8 +9,8 @@
 const float X_START_SHIFT = -4.0f;
 const float Y_START_SHIFT = -2.0f;
 
-const float LIN_SHIFT     = 0.1; // 10%
-const float SCALE_SHIFT   = 0.1; // 10%
+const float LIN_SHIFT     = 0.1;
+const float SCALE_SHIFT   = 0.1;
 
 const float MAX_RADIUS    = 4.0f;
 
@@ -18,8 +18,6 @@ const float dx = MAX_RADIUS / WIDTH;
 const float dy = MAX_RADIUS / HEIGHT;
 
 const size_t TEST = 300;
-
-typedef renderError (*colorTheme)(uint8_t *, const float, const float, const float);
 
 renderError calculateMandelbrot(sf::RenderWindow *window, uint8_t *points, const float  xShift, const float  yShift, const float  scale);
 renderError drawMandelbrotSet  (sf::RenderWindow *window, uint8_t *points);
--- src/avxMandelbrot.cpp
+++ src/avxMandelbrot.cpp
@@ -1,7 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <immintrin.h>
 #include <math.h>
-
+#include <stdbool.h>
 #include <thread>
 #include <vector>
 
@@ -10,26 +10,21 @@
 
 //////////////////////////////////// COLORS ////////////////////////////////////////////
 
-static colorTheme   COLOR_THEME[]     = {theme_0, theme_1, theme_2};
-static const size_t MAX_COLOR_THEME   = sizeof(COLOR_THEME) / sizeof(COLOR_THEME[0]);
-
-static size_t       COLOR_THEME_INDEX = 0;
-static colorTheme   setTheme          = COLOR_THEME[COLOR_THEME_INDEX];
+extern colorTheme   COLOR_THEME[];
+extern colorTheme   setTheme;
+extern const size_t MAX_COLOR_THEME;
+extern       size_t COLOR_THEME_INDEX;
 
 ////////////////////////////////////////////////////////////////////////////////////////
+
+static bool SHOW_FPS = true;
 
 void calculateMandelbrotSection(uint8_t *points, const float xShift, const float yShift, const float scale, size_t startY, size_t endY) {
     const float dx = MAX_RADIUS / WIDTH;
     static const __m256 R2max  = _mm256_set1_ps(MAX_RADIUS);
-    // [MAX_RADIUS MAX_RADIUS MAX_RADIUS MAX_RADIUS MAX_RADIUS MAX_RADIUS MAX_RADIUS MAX_RADIUS]
+    
     static const __m256 POINTS = _mm256_set_ps(7.f, 6.f, 5.f, 4.f, 3.f, 2.f, 1.f, 0.f);
-    // [7.f 6.f 5.f 4.f 3.f 2.f 1.f 0.f]
-
-    // x, y - pixels, x / WIDTH, y / HEIGHT -> [0, 1]
-    // mandelbrot center - 0.0, -> [-0.5, 0.5] for symmetry
-    // [-0.5, 0.5] -> [-0.5 * MAX_RADIUS, 0.5 * MAX_RADIUS] then scale
-    // shift image center
-
+    
     for (size_t y = startY; y < endY; y++) {
         float y0 = (y / (float) HEIGHT - 0.5f) * MAX_RADIUS * scale + yShift;
 
@@ -37,9 +32,7 @@ void calculateMandelbrotSection(uint8_t *points, const float xShift, const float
             float x0 = (x / (float) WIDTH - 0.5f) * MAX_RADIUS * scale + xShift;
 
             __m256 X0 = _mm256_add_ps(_mm256_set1_ps(x0), _mm256_mul_ps(POINTS, _mm256_set1_ps(dx * scale)));
-            // [x0 + 0*dx x0 + 1*dx x0 + 2*dx ... x0 + 7*dx]
             __m256 Y0 = _mm256_set1_ps(y0);
-            // [y0 y0 y0 y0 y0 y0 y0 y0]
 
             __m256 X = X0;
             __m256 Y = Y0;
@@ -48,45 +41,30 @@ void calculateMandelbrotSection(uint8_t *points, const float xShift, const float
 
             for (size_t iteration = 0; iteration < ITER_MAX; iteration++) {
                 __m256 X2 = _mm256_mul_ps(X, X);
-                // [(x0+0*dx)^2 (x0+1*dx)^2 (x0+2*dx)^2 (x0+3*dx)^2 (x0+4*dx)^2 (x0+5*dx)^2 (x0+6*dx)^2 (x0+7*dx)^2]
                 __m256 Y2 = _mm256_mul_ps(Y, Y);
-                // [y0^2 y0^2 y0^2 y0^2 y0^2 y0^2 y0^2 y0^2]
                 __m256 XY = _mm256_mul_ps(X, Y);
-                // [(x0+0*dx)y0 (x0+1*dx)y0 (x0+2*dx)y0 (x0+3*dx)y0 (x0+4*dx)y0 (x0+5*dx)y0 (x0+6*dx)y0 (x0+7*dx)y0]
 
                 __m256 R2  = _mm256_add_ps(X2, Y2);
                 __m256 cmp = _mm256_cmp_ps(R2, R2max, _CMP_LE_OQ);
-                // R2 <= R2max ? 0xffffffff : 0 (by element)
 
                 int mask = _mm256_movemask_ps(cmp);
-                // bit mask
-                // ex. cmp = [0xffffffff 0 0xffffffff 0 ...]
-                // mask = 0b10101010
 
                 if (!mask) {
                     break;
                 }
 
                 __m256i ones = _mm256_set1_epi32(1);
-                // [1 1 1 1 1 1 1 1]
+                
                 iterations = _mm256_add_epi32(iterations, _mm256_and_si256(_mm256_castps_si256(cmp), ones));
-                // int32_t + int32_t
-                // _mm256_castps_si256 : __m256 (float) to __m256i (int)
-                // _mm256_and_si256    : bitwise and between cmp and ones
 
                 X = _mm256_add_ps(_mm256_sub_ps(X2, Y2), X0);
-                // X = X^2 - Y^2 + X0
                 Y = _mm256_add_ps(_mm256_add_ps(XY, XY), Y0);
-                // Y = 2XY + Y0
 
-                // prefetch next data
                 _mm_prefetch((const char*)&points[(y * WIDTH + x + 8) * 4], _MM_HINT_T0);
             }
 
             int iter[8] = {};
             _mm256_storeu_si256((__m256i*)iter, iterations);
-            // save 256 bit (8 * int32_t) from avx into memory (to iter)
-            // storeu <=> unaligned (no alignment)
 
             for (size_t i = 0; i < 8 && x + i < WIDTH; i++) {
                 setTheme(points, x + i, y, (float) iter[i]);
@@ -103,7 +81,6 @@ renderError calculateMandelbrot(sf::RenderWindow *window, uint8_t *points, const
     std::vector<std::thread> threads;
     size_t rowsPerThread = HEIGHT / numThreads;
 
-    // line-by-line processing
     for (size_t i = 0; i < numThreads; i++) {
         size_t startY = i * rowsPerThread;
         size_t endY   = (i == numThreads - 1) ? HEIGHT : startY + rowsPerThread;
@@ -156,6 +133,9 @@ renderError handleKeyboard(const std::optional<sf::Event> event, float *xShift, 
                 setTheme = COLOR_THEME[COLOR_THEME_INDEX % MAX_COLOR_THEME];
                 break;
 
+            case sf::Keyboard::Key::F11:
+                SHOW_FPS = (SHOW_FPS) ? false : true;    
+
             default:
                 break;
         }
@@ -185,13 +165,18 @@ renderError drawMandelbrotSet(sf::RenderWindow *window, uint8_t *points) {
 
 int main() {
     sf::RenderWindow window(sf::VideoMode({WIDTH, HEIGHT}), "Mandelbrot Set");
-
+    
     float xShift  = -0.8;
     float yShift  = 0.0;
-    float scale   = 0.5;
+    float scale   = 1.0;
+
+    sf::Clock clock;
+
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(60);
 
     uint64_t time = 0;
-    size_t run    = 0;
+    size_t   run  = 0;
 
     sf::Font font;
 
@@ -201,10 +186,8 @@ int main() {
 
     sf::Text fpsText(font);
 
-    fpsText.setCharacterSize(20);
+    fpsText.setCharacterSize(24);
     fpsText.setFillColor(sf::Color::Red);
-
-    sf::Clock clock;
 
     float FPS = 0.0f;
 
@@ -222,8 +205,6 @@ int main() {
 
         fpsText.setString("FPS: " + std::to_string(static_cast<int>(FPS)));
 
-        window.clear();
-
         uint64_t start  = __rdtsc();
 
         uint8_t *points = (uint8_t *)calloc(WIDTH * HEIGHT * 4, sizeof(uint8_t));
@@ -234,22 +215,24 @@ int main() {
         time += end - start;
         run++;
 
-        #ifdef _test
+        #ifdef ON_TEST_
             if (run >= TEST) {
                 window.close();
             }
 
-        printf("%ld/%ld\n", run, TEST);
+            printf("%ld/%ld\n", run, TEST);
 
         #else
             drawMandelbrotSet(&window, points);
         #endif
 
-        window.draw(fpsText);
-        window.display();
+        if (SHOW_FPS) {
+            window.draw(fpsText);
+            window.display();
+        }
     }
 
-    #if _test
+    #if ON_TEST_
         printf("RUNS: %ld, TIME: %ld (ticks) , TIME PER RUN: %ld (ticks per run) \n", run, time, time / run);
     #endif
 
